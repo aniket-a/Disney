173a99b727ea9284bf6f985cd795d596ad399794
aniket-a <guptaniket962@gmail.com>
2023-09-13 17:30:59 +0530

    header initialize

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react"
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from "./components/Login"
+import Header from "./components/Header"
 
 function App() {
 
   return (
    <div className="App">
     <BrowserRouter >
+      <Header />
         <Routes>
           <Route exact path="/" element={<Login/>} />
         </Routes>
